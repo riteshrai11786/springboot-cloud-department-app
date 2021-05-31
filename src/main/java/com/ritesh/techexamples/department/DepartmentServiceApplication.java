1fa303eedb4a705d0597af6bad8389f0745e05f8
Ritesh <riteshrai11786@gmail.com>
2021-05-31 19:07:26 +0530

    Enabling eureka client

--- src/main/java/com/ritesh/techexamples/department/DepartmentServiceApplication.java
+++ src/main/java/com/ritesh/techexamples/department/DepartmentServiceApplication.java
@@ -3,11 +3,13 @@ package com.ritesh.techexamples.department;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @Slf4j
+@EnableEurekaClient
 public class DepartmentServiceApplication {
 
 	public static void main(String[] args) {
